843133f1935c2134de9e6f60b0d0f46b44083794
Ravi <ravi.yadav1983@gmail.com>
2025-02-17 15:17:18 +0530

    Add Terraform setup with GitHub Actions

--- main.tf
+++ main.tf
@@ -2,7 +2,17 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+
+
+provider "azurerm" {
+  client_id       = "33c3ef55-4dc3-44c7-8131-bebc98031f41"
+  client_secret   = "hG.8Q~OOSrtG5e_4J1DMMcdo~2lza2BfIiEknbGX"
+  tenant_id       = "b4329df8-87bd-421e-941c-60801f6c2a51"
+  subscription_id = "a5fdfee4-50cb-4ebc-9681-a8e2a2349fe5"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup4"
+  location = "westus2"
 }
