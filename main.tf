d365b6212d0fd75ae452c6ca9f0a937de0008ecd
Ravi <ravi.yadav1983@gmail.com>
2025-02-17 15:19:11 +0530

    Add Terraform setup with GitHub Actions

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 provider "azurerm" {
-  features {}
-}
-
-
-
-provider "azurerm" {
   client_id       = "33c3ef55-4dc3-44c7-8131-bebc98031f41"
   client_secret   = "hG.8Q~OOSrtG5e_4J1DMMcdo~2lza2BfIiEknbGX"
   tenant_id       = "b4329df8-87bd-421e-941c-60801f6c2a51"
