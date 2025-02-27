660f3095ff4eff182ef1ab6f9cd43208d03e0a1c
Ravi <ravi.yadav1983@gmail.com>
2025-02-27 13:51:10 +0530

    Use our HCP Terraform organization

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ terraform {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+    tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+}
 }
 
 
